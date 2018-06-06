a19aa77848940c9fcad05a3f13f653f8dca4ce18
privet-kitty <privet.kitty99@gmail.com>
2018-06-06 16:52:23 +0200

    rewrite delta-E with functional structure

--- src/core/deltae.lisp
+++ src/core/deltae.lisp
@@ -44,8 +44,7 @@
 	     ,@(extract sub-args)
 	     :illuminant (rgbspace-illuminant rgbspace)))))))
 
-
-(defdeltae deltaeab (l1 a1 b1 l2 a2 b2)
+(define-primary-functional (lab-deltaeab lab :term deltaeab) (l1 a1 b1 l2 a2 b2)
   "CIE 1976. Euclidean distance in L*a*b* space."
   (declare (optimize (speed 3) (safety 1))
            (real l1 a1 b1 l2 a2 b2))
@@ -56,9 +55,11 @@
 	     (* deltaa deltaa)
 	     (* deltab deltab)))))
 
+(define-secondary-functional xyz-deltaeab deltaeab xyz)
+(define-secondary-functional qrgb-deltaeab deltaeab qrgb)
 
 
-(defdeltae deltae94 (l1 a1 b1 l2 a2 b2 &key (application :graphic-arts))
+(define-primary-functional (lab-deltae94 lab :term deltae94) (l1 a1 b1 l2 a2 b2 &key (application :graphic-arts))
   "CIE 1994. 
 APPLICATION::= :graphic-arts | :textiles"
   (declare (optimize (speed 3) (safety 1)))
@@ -86,74 +87,81 @@ APPLICATION::= :graphic-arts | :textiles"
 		       (* term2 term2)
 		       (* term3 term3))))))))))
 
+(define-secondary-functional xyz-deltae94 deltae94 xyz)
+(define-secondary-functional qrgb-deltae94 deltae94 qrgb)
 
-(defdeltae deltae00 (l1 a1 b1 l2 a2 b2)
+
+(define-primary-functional (lab-deltae00 lab :term deltae00) (l1 a1 b1 l2 a2 b2)
   "CIEDE2000."
   (declare (optimize (speed 3) (safety 1)))
   (with-double-float (l1 a1 b1 l2 a2 b2)
-    (let ((c1 (sqrt (+ (* a1 a1) (* b1 b1))))
-	  (c2 (sqrt (+ (* a2 a2) (* b2 b2)))))
-      (let* ((deltaLprime (- l2 l1))
-	     (Lmean (* 0.5d0 (+ l1 l2)))
-	     (Cmean (* 0.5d0 (+ c1 c2)))
-	     (Cmean7 (the (double-float 0d0) (expt Cmean 7)))
-	     (const1 (* 0.5d0 (- 1d0 (sqrt (/ Cmean7 (+ Cmean7 #.(expt 25 7)))))))
-	     (aprime1 (+ a1 (* a1 const1)))
-	     (aprime2 (+ a2 (* a2 const1)))
-	     (Cprime1 (sqrt (+ (* aprime1 aprime1) (* b1 b1))))
-	     (Cprime2 (sqrt (+ (* aprime2 aprime2) (* b2 b2))))
-	     (Cmeanprime (* 0.5d0 (+ Cprime1 Cprime2)))
-	     (deltaCprime (- Cprime2 Cprime1))
-	     (hprime1 (if (and (= b1 0) (= aprime1 0))
-			  0d0
-			  (mod (* (atan b1 aprime1) #.(/ 180d0 PI)) 360d0)))
-	     (hprime2 (if (and (= b2 0) (= aprime2 0))
-			  0d0
-			  (mod (* (atan b2 aprime2) #.(/ 180d0 PI)) 360d0)))
-	     (deltahprime (cond ((or (= Cprime1 0d0) (= Cprime2 0d0))
-				 0d0)
-				((<= (abs (- hprime1 hprime2)) 180d0)
-				 (- hprime2 hprime1))
-				((<= hprime2 hprime1)
-				 (+ (- hprime2 hprime1) 360d0))
-				(t
-				 (- hprime2 hprime1 360))))
-	     (deltalargeHprime (* 2d0
-				  (sqrt (* Cprime1 Cprime2))
-				  (sin (* deltahprime #.(* 0.5d0 PI 1/180)))))
-	     (Hmeanprime (cond ((or (= Cprime1 0) (= Cprime2 0))
-			       (+ hprime1 hprime2))
-			      ((<= (abs (- hprime1 hprime2)) 180d0)
-			       (* 0.5d0 (+ hprime1 hprime2)))
-			      ((< (+ hprime1 hprime2) 360d0)
-			       (* 0.5d0 (+ hprime1 hprime2 360d0)))
-			      (t
-			       (* 0.5d0 (+ hprime1 hprime2 -360d0)))))
-	     (varT (+ 1d0
-		      (* -0.17d0 (cos (* (- Hmeanprime 30) #.(/ PI 180d0))))
-		      (* 0.24d0 (cos (* 2d0 Hmeanprime #.(/ PI 180d0))))
-		      (* 0.32d0 (cos (* (+ (* 3d0 Hmeanprime) 6d0) #.(/ PI 180))))
-		      (* -0.20d0 (cos (* (- (* 4d0 Hmeanprime) 63d0) #.(/ PI 180))))))
-	     (Lmean-50 (- Lmean 50d0))
-	     (tmp (* Lmean-50 Lmean-50))
-	     (varSL (+ 1d0 (/ (* 0.015d0 tmp)
-			      (sqrt (+ 20d0 tmp)))))
-	     (varSC (+ 1d0 (* 0.045d0 Cmeanprime)))
-	     (varSH (+ 1d0 (* 0.015d0 Cmeanprime varT)))
-	     (Cmeanprime7 (the (double-float 0d0) (expt Cmeanprime 7)))
-	     (varRT  (* -2d0
-			(sqrt (/ Cmeanprime7 (+ Cmeanprime7 #.(expt 25 7))))
-			(sin (* 60d0
-				(exp (- (expt (* (- Hmeanprime 275d0) 1/25) 2)))
-				#.(/ PI 180))))))
-	(sqrt (the (double-float 0d0)
-		   (+ (/ (* deltaLprime deltaLprime)
-			 (* varSL varSL))
-		      (/ (* deltaCprime deltaCPrime)
-			 (* varSC varSC))
-		      (/ (* deltalargeHprime deltalargeHprime)
-			 (* varSH varSH))
-		      (* varRT (/ deltaCprime varSC) (/ deltalargeHprime varSH)))))))))
+    (let* ((c1 (sqrt (+ (* a1 a1) (* b1 b1))))
+           (c2 (sqrt (+ (* a2 a2) (* b2 b2))))
+           (deltaLprime (- l2 l1))
+           (Lmean (* 0.5d0 (+ l1 l2)))
+           (Cmean (* 0.5d0 (+ c1 c2)))
+           (Cmean7 (the (double-float 0d0) (expt Cmean 7)))
+           (const1 (* 0.5d0 (- 1d0 (sqrt (/ Cmean7 (+ Cmean7 #.(expt 25 7)))))))
+           (aprime1 (+ a1 (* a1 const1)))
+           (aprime2 (+ a2 (* a2 const1)))
+           (Cprime1 (sqrt (+ (* aprime1 aprime1) (* b1 b1))))
+           (Cprime2 (sqrt (+ (* aprime2 aprime2) (* b2 b2))))
+           (Cmeanprime (* 0.5d0 (+ Cprime1 Cprime2)))
+           (deltaCprime (- Cprime2 Cprime1))
+           (hprime1 (if (and (= b1 0) (= aprime1 0))
+                        0d0
+                        (mod (* (atan b1 aprime1) #.(/ 180d0 PI)) 360d0)))
+           (hprime2 (if (and (= b2 0) (= aprime2 0))
+                        0d0
+                        (mod (* (atan b2 aprime2) #.(/ 180d0 PI)) 360d0)))
+           (deltahprime (cond ((or (= Cprime1 0d0) (= Cprime2 0d0))
+                               0d0)
+                              ((<= (abs (- hprime1 hprime2)) 180d0)
+                               (- hprime2 hprime1))
+                              ((<= hprime2 hprime1)
+                               (+ (- hprime2 hprime1) 360d0))
+                              (t
+                               (- hprime2 hprime1 360))))
+           (deltalargeHprime (* 2d0
+                                (sqrt (* Cprime1 Cprime2))
+                                (sin (* deltahprime #.(* 0.5d0 PI 1/180)))))
+           (Hmeanprime (cond ((or (= Cprime1 0) (= Cprime2 0))
+                              (+ hprime1 hprime2))
+                             ((<= (abs (- hprime1 hprime2)) 180d0)
+                              (* 0.5d0 (+ hprime1 hprime2)))
+                             ((< (+ hprime1 hprime2) 360d0)
+                              (* 0.5d0 (+ hprime1 hprime2 360d0)))
+                             (t
+                              (* 0.5d0 (+ hprime1 hprime2 -360d0)))))
+           (varT (+ 1d0
+                    (* -0.17d0 (cos (* (- Hmeanprime 30) #.(/ PI 180d0))))
+                    (* 0.24d0 (cos (* 2d0 Hmeanprime #.(/ PI 180d0))))
+                    (* 0.32d0 (cos (* (+ (* 3d0 Hmeanprime) 6d0) #.(/ PI 180))))
+                    (* -0.20d0 (cos (* (- (* 4d0 Hmeanprime) 63d0) #.(/ PI 180))))))
+           (Lmean-50 (- Lmean 50d0))
+           (tmp (* Lmean-50 Lmean-50))
+           (varSL (+ 1d0 (/ (* 0.015d0 tmp)
+                            (sqrt (+ 20d0 tmp)))))
+           (varSC (+ 1d0 (* 0.045d0 Cmeanprime)))
+           (varSH (+ 1d0 (* 0.015d0 Cmeanprime varT)))
+           (Cmeanprime7 (the (double-float 0d0) (expt Cmeanprime 7)))
+           (varRT  (* -2d0
+                      (sqrt (/ Cmeanprime7 (+ Cmeanprime7 #.(expt 25 7))))
+                      (sin (* 60d0
+                              (exp (- (expt (* (- Hmeanprime 275d0) 1/25) 2)))
+                              #.(/ PI 180))))))
+      (sqrt (the (double-float 0d0)
+                 (+ (/ (* deltaLprime deltaLprime)
+                       (* varSL varSL))
+                    (/ (* deltaCprime deltaCPrime)
+                       (* varSC varSC))
+                    (/ (* deltalargeHprime deltalargeHprime)
+                       (* varSH varSH))
+                    (* varRT (/ deltaCprime varSC) (/ deltalargeHprime varSH))))))))
+
+(define-secondary-functional xyz-deltae00 deltae00 xyz)
+(define-secondary-functional qrgb-deltae00 deltae00 qrgb)
+
 
 
 (defun bench-deltae00 (&optional (num 1000000))
